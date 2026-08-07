287199b0e57d97a832910503041529115969e039
Lucy <1970492901@qq.com>
2026-08-07 18:07:47 +0800

    更新

---
+++ Assets/HotResorces/Scripts/Moudle/Skills/SkillFrame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillFrame : MonoBehaviour
+{
+    [SerializeField] private Image img_skill;
+    [SerializeField] private Image img_bg;
+    [SerializeField] private Text  skillName;
+}
